26e83d70dc8d0d3d53ba4931b95a075b917abcbc
Mark Chang <mark.chang@vpon.com>
2021-12-18 15:07:08 +0800

    add put endpoint

--- app/main.py
+++ app/main.py
@@ -2,8 +2,14 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offter: Optional[bool] = None
 
 @app.get("/")
 async def read_root():
@@ -12,3 +18,7 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
